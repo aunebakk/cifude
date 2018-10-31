51a039a05f2a7d252c16912cccdec1e83a79ba09
Tiney01 <tiney01@outlook.com>
2018-10-31 20:21:16 +0100

    tiney here

--- tine_cache.ps1
+++ tine_cache.ps1
@@ -3,6 +3,7 @@
 # http://sql2x.azurewebsites.net/
 # Generated Date: 10/18/2018 5:33:55 AM
 # Template: sql2x.PowerShellGenerator.CifudeScript
+# Tiney here 
 <#
 .\tine_cache.ps1 -doEcho       -doSql2x -comment test -create
 .\tine_cache.ps1 -doEcho       -doSql2x -comment test -insert
