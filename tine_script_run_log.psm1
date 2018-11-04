c6c1ab4f8af8924d472dc22efd76227abd235390
aunebakk <aunebakk@hotmail.com>
2018-11-04 08:14:14 +0100

    using modules for classes and enums

---
+++ tine_script_run_log.psm1
@@ -0,0 +1,124 @@
+# SQL2X Generated code based on a SQL Server Schema
+# SQL2X Version: 0.d
+# http://sql2x.azurewebsites.net/
+# Generated Date: 11/4/2018 6:35:35 AM
+# Template: sql2x.PowerShellGenerator.CifudeScriptModule
+##################################################################################################################
+$script:taskName = 'data class'
+#region
+##################################################################################################################
+class TineScriptRunLogData {
+    [System.Guid]$tineScriptRunLogId = '00000000-0000-0000-0000-000000000000'
+    [System.String]$tineScriptRunLogPartitionKey = $env:ComputerName
+    [System.String]$tineScriptRunLogRowKey = [System.DateTime]::UtcNow.ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
+    [System.Guid]$tineTaskId = '00000000-0000-0000-0000-000000000000'
+    [System.Guid]$tineMachineId = '00000000-0000-0000-0000-000000000000'
+    [System.Guid]$tineCreationId = '00000000-0000-0000-0000-000000000000'
+    [System.String]$tineTaskScriptName = ''
+    [System.String]$tineScriptRunLogStatusRcd = ''
+    [System.DateTime]$tineTaskScriptStartDateTime = '1601.01.01T00:00:00Z'
+    [System.DateTime]$tineTaskScriptEndDateTime = '1601.01.01T00:00:00Z'
+    [System.String]$tineTaskScriptOutcomeRcd = ''
+    [System.Guid]$userId = '00000000-0000-0000-0000-000000000000'
+    [System.DateTime]$dateTime = '1601.01.01T00:00:00Z'
+}
+#endregion
+##################################################################################################################
+$script:taskName = 'refs'
+#region
+##################################################################################################################
+if (-not ([System.Management.Automation.PSTypeName]'TineCacheRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineCacheRef
+   {
+    HitThreshold,
+    None,
+    TineHalt
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineScriptReadinessStateRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineScriptReadinessStateRef
+   {
+    None,
+    Tiney
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineScriptRunLogStatusRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineScriptRunLogStatusRef
+   {
+    Cleaned,
+    Executed,
+    None,
+    Planned,
+    Searched
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskRef
+   {
+    None,
+    Script,
+    Task
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskScriptMachineOutcomeRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskScriptMachineOutcomeRef
+   {
+    Failure,
+    None,
+    ScriptFileMissing,
+    Success
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskScriptOutcomeRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskScriptOutcomeRef
+   {
+    Failure,
+    None,
+    ScriptFileMissing,
+    Success
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskStatusRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskStatusRef
+   {
+    Cleaned,
+    Executed,
+    None,
+    Planned,
+    ReTasked,
+    Searched
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTestOutcomeRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTestOutcomeRef
+   {
+    Failure,
+    None,
+    Success
+   }
+"@
+}
+
+#endregion
---
+++ tine_task_script_outcome_ref.psm1
@@ -0,0 +1,117 @@
+# SQL2X Generated code based on a SQL Server Schema
+# SQL2X Version: 0.d
+# http://sql2x.azurewebsites.net/
+# Generated Date: 11/4/2018 6:35:48 AM
+# Template: sql2x.PowerShellGenerator.CifudeScriptModule
+##################################################################################################################
+$script:taskName = 'data class'
+#region
+##################################################################################################################
+class TineTaskScriptOutcomeRefData {
+    [System.String]$tineTaskScriptOutcomeRcd = ''
+    [System.String]$tineTaskScriptOutcomeRefPartitionKey = $env:ComputerName
+    [System.String]$tineTaskScriptOutcomeRefRowKey = [System.DateTime]::UtcNow.ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
+    [System.String]$tineTaskScriptOutcomeName = ''
+    [System.Guid]$userId = '00000000-0000-0000-0000-000000000000'
+    [System.DateTime]$dateTime = '1601.01.01T00:00:00Z'
+}
+#endregion
+##################################################################################################################
+$script:taskName = 'refs'
+#region
+##################################################################################################################
+if (-not ([System.Management.Automation.PSTypeName]'TineCacheRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineCacheRef
+   {
+    HitThreshold,
+    None,
+    TineHalt
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineScriptReadinessStateRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineScriptReadinessStateRef
+   {
+    None,
+    Tiney
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineScriptRunLogStatusRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineScriptRunLogStatusRef
+   {
+    Cleaned,
+    Executed,
+    None,
+    Planned,
+    Searched
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskRef
+   {
+    None,
+    Script,
+    Task
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskScriptMachineOutcomeRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskScriptMachineOutcomeRef
+   {
+    Failure,
+    None,
+    ScriptFileMissing,
+    Success
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskScriptOutcomeRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskScriptOutcomeRef
+   {
+    Failure,
+    None,
+    ScriptFileMissing,
+    Success
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTaskStatusRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTaskStatusRef
+   {
+    Cleaned,
+    Executed,
+    None,
+    Planned,
+    ReTasked,
+    Searched
+   }
+"@
+}
+
+if (-not ([System.Management.Automation.PSTypeName]'TineTestOutcomeRef').Type) {
+   Add-Type -TypeDefinition @"
+   public enum TineTestOutcomeRef
+   {
+    Failure,
+    None,
+    Success
+   }
+"@
+}
+
+#endregion
